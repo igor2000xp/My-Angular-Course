50fe13549fee0d15e196ad878e626c7e9f4021ae
Yuri Simonov <yura15-96@mail.ru>
2024-11-17 14:54:04 +0300

    39. Providers в Angular useValue, useClass, useFactory, useExisting и класс InjectionToken

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,75 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ChildComponent } from './components/child/child.component';
-import { AsyncPipe } from '@angular/common';
-import { DataService } from './services/data.service';
-import { Observable, of } from 'rxjs';
 import {
-    HttpClient,
-    HttpEvent,
-    HttpHandler,
-    HttpRequest,
-    HttpResponse,
-} from '@angular/common/http';
+    ChangeDetectionStrategy,
+    Component,
+    inject,
+    Inject,
+} from '@angular/core';
+import { DataService } from './services/data.service';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { UserService } from './services/user.service';
+import {
+    ADMIN_RANDOM_SERVICE_TOKEN,
+    TOKEN,
+    USER_RANDOM_SERVICE_TOKEN,
+} from './tokens/token';
+import { ChildComponent } from './components/child/child.component';
 import { RandomService } from './services/random.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [ChildComponent, AsyncPipe],
     templateUrl: './app.component.html',
+    imports: [AsyncPipe, ChildComponent],
     changeDetection: ChangeDetectionStrategy.OnPush,
-    providers: [RandomService],
+    providers: [
+        DataService,
+        // строка
+        { provide: TOKEN, useValue: 'Какие-то строковые данные', multi: true },
+        // число
+        { provide: TOKEN, useValue: 1, multi: true },
+        // булево
+        { provide: TOKEN, useValue: true, multi: true },
+        // объект
+        { provide: TOKEN, useValue: { name: 'Вася' }, multi: true },
+        // массив
+        { provide: TOKEN, useValue: [1, 2, 3], multi: true },
+    ],
 })
 export class AppComponent {
     users$: Observable<any>;
 
+    dataService3 = inject(DataService);
+
     constructor(
         private dataService: DataService,
-        private randomService: RandomService
+        @Inject(TOKEN) private token: string // @Inject(DataService) private dataService2: DataService, // @Inject(USER_RANDOM_SERVICE_TOKEN) // private userRandomService: string, // @Inject(ADMIN_RANDOM_SERVICE_TOKEN) private adminRandomService: string
     ) {
         this.users$ = this.dataService.getUsers();
-        console.log(this.randomService.getRandomNumber());
+        console.log('this.token', this.token);
+        // console.log('this.dataService', this.dataService);
+        // console.log('this.dataService2', this.dataService2);
+        // console.log('this.dataService3', this.dataService3);
+        // console.log('this.userRandomService', this.userRandomService);
+        // console.log('this.adminRandomService', this.adminRandomService);
     }
-
-    // dataService: DataService;
-
-    // constructor() {
-    //     this.dataService = new DataService(new HttpClient(new Handler()));
-    //     this.users$ = this.dataService.getUsers();
-    // }
 }
 
-// class Handler implements HttpHandler {
-//     handle(req: HttpRequest<any>): Observable<HttpEvent<any>> {
-//         // Имитация ответа сервера с данными пользователей
-//         const fakeResponse = new HttpResponse({
-//             status: 200,
-//             body: [{ name: 'Вася' }, { name: 'Петя' }],
-//         });
-//         // Возвращаем Observable с нашим фейковым ответом
-//         return of(fakeResponse);
+// class Injector {
+//     private container = new Map();
+
+//     constructor(private providers: any[] = []) {
+//         this.providers.forEach((service) =>
+//             this.container.set(service, new service())
+//         );
+//     }
+
+//     get(service: any) {
+//         const serviceInstance = this.container.get(service);
+
+//         if (!serviceInstance) throw new Error('Service not provided');
+
+//         return serviceInstance;
 //     }
 // }
