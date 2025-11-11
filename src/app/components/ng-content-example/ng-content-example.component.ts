6b05681bb0a9ed5502613b5f3dac361db992de21
igor2000xp <igorring@google.com>
2025-11-11 21:38:04 +0100

    Update Angular configuration and set components and pipes as non-standalone

--- src/app/components/ng-content-example/ng-content-example.component.ts
+++ src/app/components/ng-content-example/ng-content-example.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-ng-content-example',
   templateUrl: './ng-content-example.component.html',
-  styleUrls: ['./ng-content-example.component.scss']
+  styleUrls: ['./ng-content-example.component.scss'],
+  standalone: false,
 })
 export class NgContentExampleComponent {
 
